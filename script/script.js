dfbe96b2120e231f25cafacd38740ae9f2cce87e
ashish7792 <ashish7792@gmail.com>
2017-04-17 17:53:12 +0530

    Modification for Resume

--- script/script.js
+++ script/script.js
@@ -21,7 +21,9 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main-container").style.marginLeft= "0";
-    document.body.style.backgroundColor = "white";
+    document.body.style.backgroundColor = "#53d4fd";
+
+	
 }
 
 function openModal() {
